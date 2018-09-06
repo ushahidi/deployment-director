87a3d20514ad69a8fe4df96f78356807c6a1b28c
David Losada <david@tuxpiper.com>
2018-09-06 13:03:50 +0200

    0.1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='deployment-director',
-      version='0.1.7',
+      version='0.1.8',
       description='Deployment Director',
       url='https://github.com/ushahidi/deployment-director',
       author='Ushahidi Team',
