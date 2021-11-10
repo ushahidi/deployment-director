712fbb866ac04e658ec159affc2167238878fe90
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-10 21:23:49 +0800

    Delete src/DeploymentDirector directory

--- src/DeploymentDirector/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from __future__ import absolute_import
-from .director import Context, DeploymentDirector
-import DeploymentDirector.rules as rules
-import DeploymentDirector.actions as actions
